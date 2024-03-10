d6b8a343d8a0094f6a5196a4b8086acb7a00df2b
Vladislav Tarasov <tarvlad@inbox.ru>
2024-03-11 01:21:32 +0700

    AST work

--- include/ast.h
+++ include/ast.h
@@ -4,20 +4,79 @@
 namespace yapvm {
 namespace ast {
 
-class CmpOp {};
-
-class Eq : CmpOp {};
-class NotEq : CmpOp {};
-class Lt : CmpOp {};
-class LtE : CmpOp {};
-class Gt : CmpOp {};
-class GtE : CmpOp {};
+enum CmpOpKind {
+    EQ, NOT_EQ, LT, LTE, GT, GTE
+};
 
 
+class CmpOp {
+public:
+    virtual CmpOpKind kind() const = 0;
+};
+
+class Eq : CmpOp {
+public:
+    CmpOpKind kind() const;
+};
+class NotEq : CmpOp {
+public:
+    CmpOpKind kind() const;
+};
+class Lt : CmpOp {
+public:
+    CmpOpKind kind() const;
+};
+class LtE : CmpOp {
+public:
+    CmpOpKind kind() const;
+};
+class Gt : CmpOp {
+public:
+    CmpOpKind kind() const;
+};
+class GtE : CmpOp {
+public:
+    CmpOpKind kind() const;
+};
+
+
+//TODO
 class UnaryOp {};
 
 class Invert : UnaryOp {};
 class Not : UnaryOp {};
+
+
+class Operator {};
+
+class Add : Operator {};
+class Sub : Operator {};
+class Mult : Operator {};
+class Div : Operator {};
+class Mod : Operator {};
+class Pow : Operator {};
+class LShift : Operator {};
+class RShift : Operator {};
+class BitOr : Operator {};
+class BitXor : Operator {};
+class BitAnd : Operator {};
+class FloorDiv : Operator {};
+
+
+class BoolOp {};
+
+class And : BoolOp {};
+class Or : BoolOp {};
+
+
+class ExprContext {};
+
+class Load : ExprContext {};
+class Store : ExprContext {};
+class Del : ExprContext {};
+
+
+class Expression {};
 
 }
 }
--- src/ast.cpp
+++ src/ast.cpp
@@ -1,1 +1,33 @@
 #include "ast.h"
+
+using namespace yapvm::ast;
+
+
+CmpOpKind yapvm::ast::Eq::kind() const {
+    return EQ;
+}
+
+
+CmpOpKind yapvm::ast::NotEq::kind() const {
+    return NOT_EQ;
+}
+
+
+CmpOpKind yapvm::ast::Lt::kind() const {
+    return LT;
+}
+
+
+CmpOpKind yapvm::ast::LtE::kind() const {
+    return LTE;
+}
+
+
+CmpOpKind yapvm::ast::Gt::kind() const {
+    return GT;
+}
+
+
+CmpOpKind yapvm::ast::GtE::kind() const {
+    return GTE;
+}
