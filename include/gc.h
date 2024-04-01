99f8ae42e360efb6b12960d2a44c89b11028387f
Vladislav Tarasov <tarvlad@inbox.ru>
2024-04-02 01:56:58 +0700

    Code style little refactoring

--- include/gc.h
+++ include/gc.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "y_objects.h"
-#include <vector>
 #include <deque>
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 
 namespace yapvm {
 namespace gc {
@@ -13,34 +13,34 @@ using namespace yapvm::yobjects;
 
 // wrapper for YObject
 class GCObject {
-    YObject* obj_;
+    YObject *obj_;
     bool is_marked_;
-    std::vector<GCObject*> kids_;
+    std::vector<GCObject *> kids_;
 
 public:
-    GCObject(YObject* val);
+    GCObject(YObject *val);
 
-    bool& is_marked();
-    YObject* obj();
-    std::vector<GCObject*>& kids();
+    bool &is_marked();
+    YObject *obj();
+    std::vector<GCObject *> &kids();
 };
 
 // TODO
 class Scope {
 public:
-    std::map<std::string, GCObject*> m_;
+    std::map<std::string, GCObject *> m_;
 };
 
 // TODO
 class Heap {
 public:
-    std::vector<GCObject*> objs_;
+    std::vector<GCObject *> objs_;
 };
 
 
 class GC {
-    Scope* root_;
-    Heap* heap_;
+    Scope *root_;
+    Heap *heap_;
 
 private:
     void mark();
--- src/gc.cpp
+++ src/gc.cpp
@@ -2,33 +2,33 @@
 
 using namespace yapvm::gc;
 
-GCObject::GCObject(YObject* obj)
+GCObject::GCObject(YObject *obj)
     : obj_(obj), is_marked_(false) {}
 
-bool& GCObject::is_marked() {
+bool &GCObject::is_marked() {
     return is_marked_;
 }
 
-YObject* GCObject::obj() {
+YObject *GCObject::obj() {
     return obj_;
 }
 
-std::vector<GCObject*>& GCObject::kids() {
+std::vector<GCObject *> &GCObject::kids() {
     return kids_;
 }
 
 void GC::mark() {
-    std::deque<GCObject*> deq;
-    for (const auto& [key, val] : root_->m_) {
+    std::deque<GCObject *> deq;
+    for (const auto &[key, val] : root_->m_) {
         val->is_marked() = true;
         deq.push_back(val);
     }
 
     while (!deq.empty()) {
-        GCObject* curr_obj = deq.front();
+        GCObject *curr_obj = deq.front();
         deq.pop_front();
         auto kids = curr_obj->kids();
-        for (GCObject* child : kids) {
+        for (GCObject *child : kids) {
             if (!child->is_marked()) {
                 child->is_marked() = true;
                 deq.push_back(child);
