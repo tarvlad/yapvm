158e93096cf780de45e6f9a2341686c2a2db78e4
SteveStevenpoor <geekysteve@archsteve.localdomain>
2024-04-01 17:11:38 +0700

    add gc interface

---
+++ include/gc.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "y_objects.h"
+#include <vector>
+#include <deque>
+#include <string>
+#include <map>
+
+namespace yapvm {
+namespace gc {
+
+using namespace yapvm::yobjects;
+
+// wrapper for YObject
+class GCObject {
+    YObject* obj_;
+    bool is_marked_;
+    std::vector<GCObject*> kids_;
+
+public:
+    GCObject(YObject* val);
+
+    bool& is_marked();
+    YObject* obj();
+    std::vector<GCObject*>& kids();
+};
+
+// TODO
+class Scope {
+public:
+    std::map<std::string, GCObject*> m_;
+};
+
+// TODO
+class Heap {
+public:
+    std::vector<GCObject*> objs_;
+};
+
+
+class GC {
+    Scope* root_;
+    Heap* heap_;
+
+private:
+    void mark();
+    void sweep();
+public:
+    void collect();
+};
+
+
+
+} // namespace gc
+} // namespace yapvm
