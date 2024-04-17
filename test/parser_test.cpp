6bac61b9068e3090f93ac3d18178c8ea943e5b31
Tarasov Vladislav <tarvlad@inbox.ru>
2024-04-17 21:47:17 +0700

    Refactoring for google test integration

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -1,6 +1,7 @@
 #include "ast.h"
 #include "parser.h"
 #include "utils.h"
+#include "ast_test_data.h"
 #include <gtest/gtest.h>
 
 using namespace yapvm::ast;
@@ -9,6 +10,6 @@ using namespace yapvm::parser;
 
 
 TEST(parser_test, ast_gen) {
-    //std::string test_data = read_file("test_resources/ast_test_data.txt");
-    //scoped_ptr<Module> ast = generate_ast(test_data);
+    std::string test_data = ast_txt;
+    scoped_ptr<Module> ast = generate_ast(test_data);
 }
