8cbd6c6997f010873cc1237704e4bb4e7d10547b
Tarasov Vladislav <tarvlad@inbox.ru>
2024-04-26 23:10:57 +0700

    Interpreter interface added

--- include/interpreter.h
+++ include/interpreter.h
@@ -6,4 +6,17 @@
 namespace yapvm::interpreter {
 using namespace yapvm::ast;
 
+
+class Interpreter {
+
+
+public:
+    Interpreter(Module *code);
+
+    void park();
+    bool is_parked();
+    void run();
+};
+
+
 }
