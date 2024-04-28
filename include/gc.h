a0bbe9d3f106d4efa3c518aaccb1b7f9445604cc
SteveStevenpoor <geekysteve@archsteve.localdomain>
2024-04-28 16:48:43 +0700

    init gc branch commit

--- include/gc.h
+++ include/gc.h
@@ -1,42 +1,34 @@
 #pragma once
 
 #include "y_objects.h"
+#include "scope.h"
+#include "thread_manager.h"
 #include <map>
 #include <string>
 #include <vector>
 
+#define GC_CASH_LIMIT 500
 
 // TODO need to register all ManagedObject allocations
 // TODO Only GC can clean ManagedObject
-// TODO add function ManagedObject *constr(YObject *value) function to GC, which only can be used
-// TODO for create ManagedObject-s. After this, owner of value is constructed ManagedObject
+
+using namespace yapvm::yobjects;
+using namespace yapvm::interpreter;
 
 namespace yapvm::ygc {
 
-using namespace yapvm::yobjects;
-
-// TODO
-class YScope {
-public:
-    std::map<std::string, YObject*> scope_;
-    YScope(const std::map<std::string, YObject*> &scope) : scope_(scope) {};
-};
-
-// TODO
-class YHeap {
-public:
-    std::vector<YObject*> objs_;
-    YHeap(const std::vector<YObject*> &objs) : objs_(objs) {};
-};
-
-
 class YGC {
-    YScope *root_;
-    YHeap *heap_;
+    ThreadManager tm_;
+    Scope *root_;
+    std::vector<ManagedObject *> left_, right_;
 public:
+    YGC(Scope *root, const ThreadManager &tm) : root_(root), tm_(tm),
+    left_(std::vector<ManagedObject *>()), right_(std::vector<ManagedObject *>()) {
+        collect();
+    };
+private:
     void mark() const;
     void sweep() const;
-    YGC(YScope *root, YHeap *heap) : root_(root), heap_(heap) {};
     void collect() const;
 };
 
--- src/gc.cpp
+++ src/gc.cpp
@@ -1,15 +1,20 @@
 #include "gc.h"
 #include <deque>
+#include <thread>
+#include <chrono>
+
+#define sleepns(val) std::this_thread::sleep_for(std::chrono::nanoseconds(val));
+#define sleepms(val) std::this_thread::sleep_for(std::chrono::milliseconds(val));
 
 using namespace yapvm::ygc;
 
-//void YGC::mark() const {
-//    std::deque<YObject*> deq;
+// void YGC::mark() const {
+//     std::deque<YObject*> deq;
 //    for (const auto &[key, val] : root_->scope_) {
 //        val->is_marked() = true;
 //        deq.push_back(val);
 //    }
-//
+
 //    while (!deq.empty()) {
 //        YObject *curr_obj = deq.front();
 //        deq.pop_front();
@@ -25,7 +30,8 @@ using namespace yapvm::ygc;
 //        }
 //        delete it;
 //    }
-//}
+// }
+
 //
 //void YGC::sweep() const {
 //    for (auto obj : heap_->objs_) {
@@ -37,10 +43,37 @@ using namespace yapvm::ygc;
 //    }
 //}
 //
-//void YGC::collect() const {
-//    mark();
-//    sweep();
-//}
+// [v1 v2 v3] [v1 v3]
+// [v1 v3] []
+
+void YGC::collect() const {
+    while (true) {
+        tm_.park_all();
+        while (!tm_.is_all_parked()) {
+            sleepns(500);
+        }
+
+        std::vector<Interpreter *> interprets = tm.get_all_interpreters();
+
+        if (interpreters.size() == 0) {
+            tm_.run_all();
+            break;
+        }
+
+        for (Interpreter * i : interpreters) {
+            left_.append_range(i->get_register_queue());
+        }
+        
+        if (left_.size() >= GC_CASH_LIMIT) {
+            mark();
+            sweep();
+        }
+        
+        tm_.run_all();
+
+        sleepms(500);
+    }
+}
 
 
 
